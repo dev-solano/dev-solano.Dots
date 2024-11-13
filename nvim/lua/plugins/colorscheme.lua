fad75b629d8f3f3155872d5e7e538f765dfa284a
dev-solano <lsolano@marfilbsc.com>
2024-11-13 12:42:59 -0500

    update ColorScheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
-	},
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 100,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
 }
