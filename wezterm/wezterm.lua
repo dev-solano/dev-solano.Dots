05f2c571381d50f9f443b177d306e578b89a26ec
dev-solano <lsolano@marfilbsc.com>
2024-11-13 12:44:36 -0500

    update wezterm.lua config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -46,21 +46,13 @@ config.colors.ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#
 config.colors.brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" }
 config.colors.indexed = { [16] = "#ffa066", [17] = "#ff5d62" }
 --  change
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.font = wezterm.font("FiraCode Nerd Font")
-config.hide_tab_bar_if_only_one_tab = true
-
--- activate ONLY if windows --
-
--- config.default_domain = 'WSL:Ubuntu'
--- config.front_end = "WebGpu"
--- config.max_fps = 120
--- for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
---	if gpu.backend == "Vulkan" then
--- 		config.webgpu_preferred_adapter = gpu
--- 		break
--- 	end
--- end
+config.font_size = 10
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+config.initial_rows = 50
+config.initial_cols = 100
 
 -- and finally, return the configuration to wezterm
 return config
